5b03b54961bdf1368aea2d8f3dbf4956700a399a
Vikram Singh <viksin19@in.ibm.com>
2019-07-29 00:20:00 +0530

    latest upadate

--- dbs/userdb.js
+++ dbs/userdb.js
@@ -1,27 +1,31 @@
 const  product = [
     {
         id: 1,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/619rQGO8ERL._SX522_.jpg',
         name: 'trimmer',
         item: 'philips',
         quantity: 10,
-        price: 100
+        price: 1000
     },
     {
         id: 2,
+        img: 'https://rukminim1.flixcart.com/image/416/416/j2ur3ww0-2/bottle/p/f/c/1000-h20-water-bottle-wb10-cello-original-imaeuyz7sbk3ycdh.jpeg?q=70',
         name: 'bottle',
         item: 'cello',
         quantity: 5,
-        price: 10
+        price: 40
     },
     {
         id: 3,
+        img: 'https://www.startech.com.bd/image/cache/catalog/laptop/lenovo/idapad-330/330-ideapad-blue-3-500x500.jpg',
         name: 'laptop',
         item: 'lenovo',
         quantity: 10,
-        price: 10000
+        price: 60000
     },
     {
         id: 4,
+        img: 'https://n4.sdlcdn.com/imgs/g/6/p/Nice-Cotton-Cotton-Double-Bedsheet-SDL751815940-1-e50cd.jpeg',
         name: 'bedsheet',
         item: 'levis',
         quantity: 20,
@@ -29,27 +33,31 @@ const  product = [
     },
     {
         id: 5,
+        img: 'https://assets.mspimages.in/c/tr:w-375,h-300,c-at_max/15385-1-1.jpg',
         name: 'mobile',
         item: 'nokia',
         quantity: 10,
-        price: 1000
+        price: 10000
     },
     {
         id: 6,
+        img: 'https://n2.sdlcdn.com/imgs/d/o/u/JBL-T450BT-On-Ear-Wireless-SDL351859776-1-3fc86.jpg',
         name: 'headphones',
-        item: 'philips',
+        item: 'JBL',
         quantity: 7,
-        price: 500
+        price: 1500
     },
     {
         id: 7,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/71QHGTKiwAL._UX569_.jpg',
         name: 'watch',
         item: 'fastrack',
         quantity: 30,
-        price: 600
+        price: 1600
     },
     {
         id: 8,
+        img: 'https://www.smallflower.com/prodimages/8938-DEFAULT-l.jpg',
         name: 'shaving cream',
         item: 'nivea',
         quantity: 10,
